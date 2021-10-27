15cedd0a2d11f14362ae8fd5dcf899940065c53f
Vyas Raina <sayvaniar@gmail.com>
2021-10-27 15:01:25 +0100

    debug

--- tsne_token_plot.py
+++ tsne_token_plot.py
@@ -84,6 +84,9 @@ if __name__ == '__main__':
     tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=args.iter)
     tsne_results = tsne.fit_transform(data)
 
+    df['tsne-2d-one'] = tsne_results[:,0]
+    df['tsne-2d-two'] = tsne_results[:,1]
+
     plt.figure(figsize=(16,10))
     sns.scatterplot(
         x="tsne-2d-one", y="tsne-2d-two",
